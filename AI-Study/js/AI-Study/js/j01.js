64afbc55ba5665649e54b247cb6040ae234df513
wing0907 <wjj9319@gmail.com>
2025-10-20 23:00:00 +0900

    import snapshot of js (2025-10-20 23:00:00 +0900)

---
+++ AI-Study/js/AI-Study/js/j01.js
@@ -0,0 +1,4 @@
+console.log('우하하하하하하하')
+console.log('우하하하하하하하2')
+console.log('우하하하하하하하3') //console 창에서 console.clear() 하면 다 지워짐
+
---
+++ AI-Study/js/AI-Study/js/j02.js
@@ -0,0 +1,20 @@
+// 1. 변수
+let age = 22;
+console.log(age);
+
+age = 30
+
+//let 은 변수
+console.log(age)
+
+//let age = 55
+//console.log(age) //변수 선언. 중복 선언하면 오류생김
+
+aaa = 33
+bbb = 44
+console.log(aaa + bbb)
+
+// 2. 상수
+const birth = '1996.01.10'
+console.log(birth)
+
